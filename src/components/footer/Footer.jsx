de1157192bc23ba2ee410d855f31b0aeb8c25c76
Amine-Triki <amine.triki15@gmail.com>
2025-05-01 19:31:44 +0100

    editing footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className="bg-blue-500 p-2  shadow  dark:bg-gray-800">
     <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span className="text-sm text-gray-100 sm:text-center dark:text-gray-400">© {year} Amine Triki || All Rights Reserved.
+      <span className="text-sm text-gray-100 sm:text-center dark:text-gray-400">© 2024 - {year} Amine Triki || All Rights Reserved.
     </span>
     <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-100 dark:text-gray-400 sm:mt-0">
         <li>
